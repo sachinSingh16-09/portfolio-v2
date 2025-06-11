bf3094ff187819fd1e609cce4d76bfe17f2d8da8
sachinSingh16-09 <sachinishu02@gmail.com>
2025-06-11 23:32:54 +0530

    Updates

---
+++ precache-manifest.d83ac1d4822a4b8f773cc813e02fa011.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4120db997b658584cfb44f182732de",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "3b4d43815274fcb871cd",
+    "url": "/portfolio-v2/static/css/2.0e31c611.chunk.css"
+  },
+  {
+    "revision": "3b4d43815274fcb871cd",
+    "url": "/portfolio-v2/static/js/2.3dc7aca4.chunk.js"
+  },
+  {
+    "revision": "a6a6f2517c7a12ff148c384e26113c97",
+    "url": "/portfolio-v2/static/js/2.3dc7aca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a8bb91428f45b1cb83",
+    "url": "/portfolio-v2/static/js/3.11a81b32.chunk.js"
+  },
+  {
+    "revision": "bf2c3e05447377d123e3",
+    "url": "/portfolio-v2/static/js/main.3eb4f0b7.chunk.js"
+  },
+  {
+    "revision": "2c7e42f3dfeffc74229b",
+    "url": "/portfolio-v2/static/js/runtime-main.97f8fa5b.js"
+  },
+  {
+    "revision": "fa239a57ecdac9704415fbb7c0b1a052",
+    "url": "/portfolio-v2/static/media/Resume_Nischal_Dwaral.fa239a57.pdf"
+  },
+  {
+    "revision": "b7b72ee27b480abe72189248cea33cae",
+    "url": "/portfolio-v2/static/media/Vector2.b7b72ee2.png"
+  },
+  {
+    "revision": "c7401f520e568a2994f0269e6c537f1d",
+    "url": "/portfolio-v2/static/media/aws.c7401f52.png"
+  },
+  {
+    "revision": "984acdaba6d70082bdf29805de96120b",
+    "url": "/portfolio-v2/static/media/coder.984acdab.webp"
+  },
+  {
+    "revision": "af96d4b7102bbd274d71e8145df66a13",
+    "url": "/portfolio-v2/static/media/github.af96d4b7.png"
+  },
+  {
+    "revision": "2c1a7560c88ea83e6b2593cd07af8ad8",
+    "url": "/portfolio-v2/static/media/gmail.2c1a7560.png"
+  },
+  {
+    "revision": "58b24d8fafb34d5bb85760994ff186a1",
+    "url": "/portfolio-v2/static/media/grafana.58b24d8f.png"
+  },
+  {
+    "revision": "6c022ca603ae2c922361c3c1a14bcfb3",
+    "url": "/portfolio-v2/static/media/jenkins.6c022ca6.webp"
+  },
+  {
+    "revision": "ea798a7abf761b1b7b965a9f16eee1a6",
+    "url": "/portfolio-v2/static/media/kuberenetes.ea798a7a.png"
+  },
+  {
+    "revision": "d492efc706db983e74258dbd348f2208",
+    "url": "/portfolio-v2/static/media/linkedin.d492efc7.png"
+  },
+  {
+    "revision": "215e3bfec4268badf069ca91af2bf43b",
+    "url": "/portfolio-v2/static/media/net-logo.215e3bfe.png"
+  },
+  {
+    "revision": "076303a788705113dc6cebc4fe59a02d",
+    "url": "/portfolio-v2/static/media/nosql.076303a7.png"
+  },
+  {
+    "revision": "7afd213071e52a3a50d7d9c9db74d29b",
+    "url": "/portfolio-v2/static/media/postman.7afd2130.png"
+  },
+  {
+    "revision": "8a65e61bd779937fb92a7db36141d1b2",
+    "url": "/portfolio-v2/static/media/redis.8a65e61b.png"
+  },
+  {
+    "revision": "0881daccb5178410b18a1a36984da095",
+    "url": "/portfolio-v2/static/media/spring-boot-logo.0881dacc.png"
+  },
+  {
+    "revision": "eb373e5d81f466262ff73eaffbc559ca",
+    "url": "/portfolio-v2/static/media/wave.eb373e5d.png"
+  }
+]);
